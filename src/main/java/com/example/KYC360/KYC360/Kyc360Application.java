5532f1c2a99798f0b74b9c873dfd19e91073ffd6
Anmol8604 <annol46740@gmail.com>
2024-02-12 23:09:53 +0530

    Project Set-Up

---
+++ src/main/java/com/example/KYC360/KYC360/Kyc360Application.java
@@ -0,0 +1,13 @@
+package com.example.KYC360.KYC360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyc360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kyc360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KYC360/KYC360/Kyc360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KYC360.KYC360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kyc360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
